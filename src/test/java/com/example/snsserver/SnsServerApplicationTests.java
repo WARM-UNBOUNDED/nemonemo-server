52c6bf990e5a154b6e52cdb5b1f1eaa783b531d4
valhub08 <jhstl1203@gmail.com>
2025-03-20 20:05:35 +0900

    first commit

---
+++ src/test/java/com/example/snsserver/SnsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
