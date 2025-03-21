16136127f439001abe25b5cfe6ffda5e8b80674b
valhub08 <jhstl1203@gmail.com>
2025-03-21 09:59:07 +0900

    feat :: LoginRequestDto

---
+++ src/main/java/com/example/snsserver/domain/dto/LoginRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.snsserver.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequestDto extends BaseMemberRequestDto {
+}
