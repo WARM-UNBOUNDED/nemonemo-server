5daa8c0c1fee2c82cf2e2bdb91246da1e08d6978
valhub08 <jhstl1203@gmail.com>
2025-03-21 09:59:38 +0900

    feat :: RefreshToken

---
+++ src/main/java/com/example/snsserver/domain/domain/RefreshToken.java
@@ -0,0 +1,25 @@
+package com.example.snsserver.domain.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String tokenValue;
+
+    public void updateValue(String tokenValue) {
+        this.tokenValue = tokenValue;
+    }
+}
