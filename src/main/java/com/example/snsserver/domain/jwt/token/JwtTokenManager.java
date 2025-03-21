166699924066cbbf9429872f2508fdb721bc82c7
valhub08 <jhstl1203@gmail.com>
2025-03-21 09:58:59 +0900

    feat :: JwtTokenManager

---
+++ src/main/java/com/example/snsserver/domain/jwt/token/JwtTokenManager.java
@@ -0,0 +1,60 @@
+package com.example.snsserver.domain.jwt.token;
+
+import com.example.snsserver.domain.jwt.constants.TokenConstants;
+import com.example.snsserver.domain.jwt.exception.TokenValidationException;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtTokenManager implements TokenGenerator, TokenValidator {
+    private final Key key;
+
+    public JwtTokenManager(@Value("${jwt.secret}") String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    @Override
+    public String generateToken(String subject, String authorities, Date expiration) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .claim(TokenConstants.AUTHORITIES_KEY, authorities)
+                .setExpiration(expiration)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public Claims parseToken(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            log.warn("Token has expired", e);
+            throw new TokenValidationException("토큰이 만료되었습니다.");
+        } catch (Exception e) {
+            log.error("Invalid token", e);
+            throw new TokenValidationException("유효하지 않은 토큰입니다.");
+        }
+    }
+
+    @Override
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            log.error("JWT validation failed", e);
+            return false;
+        }
+    }
+}
