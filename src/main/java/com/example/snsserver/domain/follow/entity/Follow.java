e41ff390b99e2e0568ae18013dbd7ab0c3ac60da
valhub08 <jhstl1203@gmail.com>
2025-03-26 08:41:41 +0900

    feat :: Follow

---
+++ src/main/java/com/example/snsserver/domain/follow/entity/Follow.java
@@ -0,0 +1,28 @@
+package com.example.snsserver.domain.follow.entity;
+
+import com.example.snsserver.domain.auth.entity.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "follows", uniqueConstraints = @UniqueConstraint(columnNames = {"follower_id", "following_id"}))
+public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id", nullable = false)
+    private Member follower;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id", nullable = false)
+    private Member following;
+}
