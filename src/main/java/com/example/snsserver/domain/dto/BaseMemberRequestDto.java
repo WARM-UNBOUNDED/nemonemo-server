d74b76e3a2b055e6592e0587e31df2dc17bac4ec
valhub08 <jhstl1203@gmail.com>
2025-03-25 13:29:53 +0900

    feat :: BaseMemberRequestDto

--- src/main/java/com/example/snsserver/domain/dto/BaseMemberRequestDto.java
+++ src/main/java/com/example/snsserver/domain/dto/BaseMemberRequestDto.java
@@ -10,13 +10,11 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public abstract class BaseMemberRequestDto {
-    @Size(min = 1, max = 10, message = "아이디는 10자리 이상 입력 불가합니다")
-    @NotBlank(message = "이름은 필수 입력값입니다.")
-    private String name;
 
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
 
+    @Size(min = 1, max = 10, message = "아이디는 10자리 이상 입력 불가합니다")
     @NotBlank(message = "아이디는 필수 입력값입니다.")
     private String username;
 }
