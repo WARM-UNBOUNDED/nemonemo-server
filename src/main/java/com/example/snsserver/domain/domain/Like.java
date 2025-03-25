b8d9540f9e42ddb59d85b0bd41c131b528bb9412
valhub08 <jhstl1203@gmail.com>
2025-03-25 14:25:49 +0900

    feat :: Like.java

---
+++ src/main/java/com/example/snsserver/domain/domain/Like.java
@@ -0,0 +1,27 @@
+package com.example.snsserver.domain.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "user_likes", uniqueConstraints = @UniqueConstraint(columnNames = {"member_id", "post_id"})) // 테이블 이름 변경
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
