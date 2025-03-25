3b418b407f453430f2026b6f3d77765b20a5c364
valhub08 <jhstl1203@gmail.com>
2025-03-25 13:30:18 +0900

    feat :: MemberResponseDto

--- src/main/java/com/example/snsserver/domain/dto/MemberResponseDto.java
+++ src/main/java/com/example/snsserver/domain/dto/MemberResponseDto.java
@@ -7,13 +7,11 @@ import lombok.Getter;
 @Getter
 public class MemberResponseDto {
     private Long id;
-    private String name;
     private String username;
     private Authority authority;
 
     public MemberResponseDto(Member member) {
         this.id = member.getId();
-        this.name = member.getName();
         this.username = member.getUsername();
         this.authority = member.getAuthority();
     }
