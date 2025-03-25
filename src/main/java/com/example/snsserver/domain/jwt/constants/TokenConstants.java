7943dcaea75914a1a8466342cb436bea8db3e9b3
valhub08 <jhstl1203@gmail.com>
2025-03-25 13:31:21 +0900

    feat :: TokenConstants

--- src/main/java/com/example/snsserver/domain/jwt/constants/TokenConstants.java
+++ src/main/java/com/example/snsserver/domain/jwt/constants/TokenConstants.java
@@ -5,6 +5,6 @@ public class TokenConstants {
     public static final String BEARER_PREFIX = "Bearer ";
     public static final String BEARER_TYPE = "Bearer";
     public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;  // 30분
-    public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24; // 24시간
+    public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; // 7일
 }
