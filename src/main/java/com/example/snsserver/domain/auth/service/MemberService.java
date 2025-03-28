07e9d001e2626df0e878b75d3fe83d4f73ea8ade
valhub08 <jhstl1203@gmail.com>
2025-03-28 14:14:09 +0900

    feat :: 회원 프로필 이미지 변경 기능 추가

--- src/main/java/com/example/snsserver/domain/auth/service/MemberService.java
+++ src/main/java/com/example/snsserver/domain/auth/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.example.snsserver.domain.auth.service;
 
+import com.example.snsserver.common.service.BaseImageService;
 import com.example.snsserver.domain.auth.repository.MemberRepository;
 import com.example.snsserver.domain.auth.entity.Member;
 import com.example.snsserver.dto.auth.response.MemberResponseDto;
@@ -8,10 +9,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService extends BaseImageService {
 
     private final MemberRepository memberRepository;
 
@@ -34,5 +36,20 @@ public class MemberService {
     public Member getMemberByUsername(String username) {
         return memberRepository.findByUsername(username)
                 .orElseThrow(() -> new IllegalArgumentException("유저 정보가 없습니다."));
+    }
+
+    @Transactional
+    public MemberResponseDto updateProfileImage(MultipartFile file) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Member member = memberRepository.findByUsername(authentication.getName())
+                .orElseThrow(() -> new IllegalArgumentException("로그인 유저 정보가 없습니다."));
+
+        deleteImage(member.getProfileImagePath());
+
+        String profileImagePath = uploadImage(file, "profile");
+        member.updateProfileImage(profileImagePath);
+
+        memberRepository.save(member);
+        return new MemberResponseDto(member);
     }
 }
