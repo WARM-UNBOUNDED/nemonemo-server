8ca25d213612015bb8aabc530b83ba4ff035a4a5
valhub08 <jhstl1203@gmail.com>
2025-04-01 10:01:35 +0900

    feat :: pageable 코드 추가

--- src/main/java/com/example/snsserver/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/snsserver/domain/comment/repository/CommentRepository.java
@@ -1,10 +1,10 @@
 package com.example.snsserver.domain.comment.repository;
 
 import com.example.snsserver.domain.comment.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPostId(Long postId);
+    Page<Comment> findByPostId(Long postId, Pageable pageable);
 }
