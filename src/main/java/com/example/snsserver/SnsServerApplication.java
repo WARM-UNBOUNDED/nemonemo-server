ce0b98fab4c933b4c4b368c1730bc41d8016c2cc
valhub08 <jhstl1203@gmail.com>
2025-04-10 19:36:46 +0900

    refactor(SnsServerApplication.java) :: JPA Auditing 제거

--- src/main/java/com/example/snsserver/SnsServerApplication.java
+++ src/main/java/com/example/snsserver/SnsServerApplication.java
@@ -2,14 +2,11 @@ package com.example.snsserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class SnsServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SnsServerApplication.class, args);
     }
-
 }
