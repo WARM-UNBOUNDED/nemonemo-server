c62f67a452aab51cd9b04f6fae17489bbb3fe799
valhub08 <jhstl1203@gmail.com>
2025-04-10 21:22:58 +0900

    delete(FileUploadConfig.java) :: 로컬 파일 업로드 제거

--- src/main/java/com/example/snsserver/config/FileUploadConfig.java
+++ src/main/java/com/example/snsserver/config/FileUploadConfig.java
@@ -1,13 +1,13 @@
-package com.example.snsserver.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FileUploadConfig {
-
-    @Bean
-    public String uploadDir() {
-        return "/Users/kmj1110/uploads";
-    }
-}
+//package com.example.snsserver.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class FileUploadConfig {
+//
+//    @Bean
+//    public String uploadDir() {
+//        return "/Users/kmj1110/uploads";
+//    }
+//}
