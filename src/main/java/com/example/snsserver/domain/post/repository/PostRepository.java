ed8501dab98214aff9518974b64ef61e3879d8fc
valhub08 <jhstl1203@gmail.com>
2025-04-10 19:35:34 +0900

    refactor(PostRepository.java) :: N+1 문제 해결 벌크 쿼리 추가

--- src/main/java/com/example/snsserver/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/snsserver/domain/post/repository/PostRepository.java
@@ -5,7 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
     Page<Post> findAll(Pageable pageable);
+
+    @Query("SELECT p.id, COUNT(l) FROM Post p LEFT JOIN Like l ON l.post = p WHERE p IN :posts GROUP BY p.id")
+    List<Object[]> countLikesByPosts(@Param("posts") List<Post> posts);
 }
