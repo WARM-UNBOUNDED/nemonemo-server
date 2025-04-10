d675b4fdbe5c91810d96925e259b98542201f297
valhub08 <jhstl1203@gmail.com>
2025-04-10 19:36:21 +0900

    refactor(MemberRepository.java) :: 명시적 쿼리 추가

--- src/main/java/com/example/snsserver/domain/auth/repository/MemberRepository.java
+++ src/main/java/com/example/snsserver/domain/auth/repository/MemberRepository.java
@@ -2,10 +2,15 @@ package com.example.snsserver.domain.auth.repository;
 
 import com.example.snsserver.domain.auth.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByUsername(String username);
+
+    @Query("SELECT m FROM Member m WHERE m.username = :username")
+    Optional<Member> findByUsername(@Param("username") String username);
+
     boolean existsByUsername(String username);
 }
