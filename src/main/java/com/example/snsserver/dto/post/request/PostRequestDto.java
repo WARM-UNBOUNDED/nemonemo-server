e6a216d577013423c346e7e4285936f86a4ebe71
valhub08 <jhstl1203@gmail.com>
2025-04-24 14:05:58 +0900

    feat(PostRequestDto.java) :: tags 필드 추가

--- src/main/java/com/example/snsserver/dto/post/request/PostRequestDto.java
+++ src/main/java/com/example/snsserver/dto/post/request/PostRequestDto.java
@@ -3,6 +3,9 @@ package com.example.snsserver.dto.post.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class PostRequestDto {
     @NotBlank(message = "제목은 필수입니다.")
@@ -10,4 +13,6 @@ public class PostRequestDto {
 
     @NotBlank(message = "내용은 필수입니다.")
     private String content;
+
+    private List<String> tags = new ArrayList<>();
 }
